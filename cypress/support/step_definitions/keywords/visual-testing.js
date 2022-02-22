961c43939fd0f159181c615d1cd3b9a299629df9
pirasanthan-jesugeevegan <pirasanth23@hotmail.com>
2022-02-22 11:43:16 +0000

    added new commands

--- cypress/support/step_definitions/keywords/visual-testing.js
+++ cypress/support/step_definitions/keywords/visual-testing.js
@@ -9,16 +9,13 @@ Given(
   'the user screenshot the section of the page as {string} from {string}',
   (value, element) => {
     cy.get(element).should('be.visible');
-    cy.eyesCheckWindow({
-      tag: value,
-      target: 'region',
-      selector: element,
+    cy.percySnapshot(value, {
+      domTransformation: (documentClone) => {
+        let bioContainer = documentClone.querySelector(element);
+        documentClone.querySelector('body').innerHTML = bioContainer.innerHTML;
+
+        return documentClone;
+      },
     });
   }
 );
-
-Given('the user initiates visual testing as {string}', (value) => {
-  cy.eyesOpen({
-    testName: value,
-  });
-});
